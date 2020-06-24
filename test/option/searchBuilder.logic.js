45c01fc061814c8b8d80a0bf3cf79a114c1daffa
Colin Marks <colin@datatables.net>
2020-06-24 09:52:40 +0100

    DEV unit tests and updates for the logic option

---
+++ test/option/searchBuilder.logic.js
@@ -0,0 +1,33 @@
+describe('searchBuilder - options - searchBuilder.logic', function() {
+	let table;
+
+	dt.libs({
+		js: ['jquery', 'datatables', 'searchbuilder'],
+		css: ['datatables', 'searchbuilder']
+	});
+
+	describe('Functional tests', function() {
+		dt.html('basic');
+		it('AND by default', function() {
+			table = $('#example').DataTable({
+				dom: 'Qlfrtip'
+			});
+
+			$('.dtsb-group button').click();
+			expect($('.dtsb-logic').text()).toBe('And');
+		});
+
+		dt.html('basic');
+		it('Can choose OR', function() {
+			table = $('#example').DataTable({
+				dom: 'Qlfrtip',
+				searchBuilder: {
+					logic: 'OR'
+				}
+			});
+
+			$('.dtsb-group button').click();
+			expect($('.dtsb-logic').text()).toBe('Or');
+		});
+	});
+});
