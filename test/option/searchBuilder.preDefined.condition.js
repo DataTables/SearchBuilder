149c77e48664141210c11d77a0a70c85c507f363
Colin Marks <colin@datatables.net>
2020-07-02 12:04:09 +0100

    DEV completed preCondition.criteria.condition now fixes in place

--- test/option/searchBuilder.preDefined.condition.js
+++ test/option/searchBuilder.preDefined.condition.js
@@ -10,6 +10,7 @@ describe('searchBuilder - options - searchBuilder.preDefined.condition', functio
 	describe('String tests', function() {
 		dt.html('basic');
 		it('... Contains', function() {
+			$('#example tbody tr:eq(2) td:eq(2)').text('');
 			table = $('#example').DataTable({
 				dom: 'Qlfrtip',
 				searchBuilder: {
@@ -18,7 +19,7 @@ describe('searchBuilder - options - searchBuilder.preDefined.condition', functio
 							{
 								condition: 'Contains',
 								data: 'Name',
-								value: ['Cox']
+								value: ['Ox']
 							}
 						],
 						logic: 'AND'
@@ -30,10 +31,8 @@ describe('searchBuilder - options - searchBuilder.preDefined.condition', functio
 			expect($('#example tbody tr td:eq(0)').text()).toBe('Ashton Cox');
 		});
 		it('... Empty', function() {
-			$('#example tbody tr:eq(2) td:eq(2)').text('');
 			table = $('#example').DataTable({
 				dom: 'Qlfrtip',
-				// DD-1566
 				destroy: true,
 				searchBuilder: {
 					preDefined: {
@@ -41,7 +40,6 @@ describe('searchBuilder - options - searchBuilder.preDefined.condition', functio
 							{
 								condition: 'Empty',
 								data: 'Office',
-								value: ['Cox']
 							}
 						],
 						logic: 'AND'
@@ -52,233 +50,250 @@ describe('searchBuilder - options - searchBuilder.preDefined.condition', functio
 			expect($('#example tbody tr').length).toBe(1);
 			expect($('#example tbody tr td:eq(0)').text()).toBe('Ashton Cox');
 		});
+		it('... Not empty', function() {
+			table = $('#example').DataTable({
+				dom: 'Qlfrtip',
+				destroy: true,
+				searchBuilder: {
+					preDefined: {
+						criteria: [
+							{
+								condition: 'Not Empty',
+								data: 'Office'
+							}
+						],
+						logic: 'AND'
+					}
+				}
+			});
 
-		// dt.html('basic'); //DD-1566
-		// it('... Not empty', function() {
-		// 	// DD-1566
-		// 	$('#example tbody tr:eq(2) td:eq(2)').text('');
-		// 	table = $('#example').DataTable({
-		// 		dom: 'Qlfrtip',
-		// 		// DD-1566
-		// 		// destroy: true,
-		// 		searchBuilder: {
-		// 			preDefined: {
-		// 				criteria: [
-		// 					{
-		// 						condition: 'Empty',
-		// 						data: 'Office',
-		// 						value: ['Cox']
-		// 					}
-		// 				],
-		// 				logic: 'AND'
-		// 			}
-		// 		}
-		// 	});
-
-		// 	expect($('#example tbody tr').length).toBe(1);
-		// 	expect($('#example tbody tr td:eq(0)').text()).toBe('Ashton Cox');
-		// });
+			expect($('#example tbody tr').length).toBe(10);
+			expect($('#example tbody tr:eq(2) td:eq(0)').text()).toBe('Bradley Greer');
+		});
 	});
 
-	// // Basic tests are in the other preDefined tests, so these are more complex
-	// describe('Numeric tests', function() {
-	// 	// Create test checks once DD-1568 fixed
-	// 	// dt.html('basic');
-	// 	// it('... Between', function() {
-	// 	// 	table = $('#example').DataTable({
-	// 	// 		dom: 'Qlfrtip',
-	// 	// 		searchBuilder: {
-	// 	// 			preDefined: {
-	// 	// 				criteria: [
-	// 	// 					{
-	// 	// 						condition: 'Between',
-	// 	// 						data: 'Age',
-	// 	// 						value: ['2', '100']
-	// 	// 					}
-	// 	// 				],
-	// 	// 				logic: 'AND'
-	// 	// 			}
-	// 	// 		}
-	// 	// 	});
-	// 	// 	// expect($('.dtsb-logic').text()).toBe('And');
-	// 	// 	// expect($('.dtsb-criteria').length).toBe(2);
-	// 	// 	// expect($('#example tbody tr td:eq(0)').text()).toBe('Ashton Cox');
-	// 	// });
+	describe('Numeric tests', function() {
+		dt.html('basic');
+		it('... Between', function() {
+			$('#example tbody tr:eq(3) td:eq(3)').text('');
+			table = $('#example').DataTable({
+				dom: 'Qlfrtip',
+				searchBuilder: {
+					preDefined: {
+						criteria: [
+							{
+								condition: 'Between',
+								data: 'Age',
+								value: ['63', '70']
+							}
+						],
+						logic: 'AND'
+					}
+				}
+			});
+			expect($('#example tbody tr').length).toBe(8);
+			expect($('#example tbody tr td:eq(0)').text()).toBe('Ashton Cox');
+		});
+		it('... Not Between', function() {
+			table = $('#example').DataTable({
+				dom: 'Qlfrtip',
+				destroy: true,
+				searchBuilder: {
+					preDefined: {
+						criteria: [
+							{
+								condition: 'Not Between',
+								data: 'Age',
+								value: ['15', '64']
+							}
+						],
+						logic: 'AND'
+					}
+				}
+			});
+			expect($('#example tbody tr').length).toBe(4);
+			expect($('#example tbody tr td:eq(0)').text()).toBe('Ashton Cox');
+		});
+		it('... Empty', function() {
+			$('#example tbody tr:eq(2) td:eq(2)').text('');
+			table = $('#example').DataTable({
+				dom: 'Qlfrtip',
+				destroy: true,
+				searchBuilder: {
+					preDefined: {
+						criteria: [
+							{
+								condition: 'Empty',
+								data: 'Age',
+							}
+						],
+						logic: 'AND'
+					}
+				}
+			});
 
-	// 	// Not Between
+			expect($('#example tbody tr').length).toBe(1);
+			expect($('#example tbody tr td:eq(0)').text()).toBe('Cedric Kelly');
+		});
+		it('... Greater than equal to', function() {
+			table = $('#example').DataTable({
+				dom: 'Qlfrtip',
+				destroy: true,
+				searchBuilder: {
+					preDefined: {
+						criteria: [
+							{
+								condition: 'Greater Than Equal To',
+								data: 'Age',
+								value: ['66']
+							}
+						],
+						logic: 'AND'
+					}
+				}
+			});
 
-	// 	dt.html('basic'); //DD-1566
-	// 	it('... Not empty', function() {
-	// 		// DD-1566
-	// 		$('#example tbody tr:eq(2) td:eq(2)').text('');
-	// 		table = $('#example').DataTable({
-	// 			dom: 'Qlfrtip',
-	// 			// DD-1566
-	// 			// destroy: true,
-	// 			searchBuilder: {
-	// 				preDefined: {
-	// 					criteria: [
-	// 						{
-	// 							condition: 'Empty',
-	// 							data: 'Office',
-	// 							value: ['Cox']
-	// 						}
-	// 					],
-	// 					logic: 'AND'
-	// 				}
-	// 			}
-	// 		});
+			expect($('#example tbody tr').length).toBe(2);
+			expect($('#example tbody tr td:eq(0)').text()).toBe('Ashton Cox');
+		});
+		it('... Greater than', function() {
+			table = $('#example').DataTable({
+				dom: 'Qlfrtip',
+				destroy: true,
+				searchBuilder: {
+					preDefined: {
+						criteria: [
+							{
+								condition: 'Greater Than',
+								data: 'Age',
+								value: ['66']
+							}
+						],
+						logic: 'AND'
+					}
+				}
+			});
 
-	// 		expect($('#example tbody tr').length).toBe(1);
-	// 		expect($('#example tbody tr td:eq(0)').text()).toBe('Ashton Cox');
-	// 	});
+			expect($('#example tbody tr').length).toBe(1);
+			expect($('#example tbody tr').text()).toBe('No matching records found');
+		});
+	});
 
-	// 	dt.html('basic'); //DD-1566
-	// 	it('... Greater than equal to', function() {
-	// 		// DD-1566
-	// 		$('#example tbody tr:eq(2) td:eq(2)').text('');
-	// 		table = $('#example').DataTable({
-	// 			dom: 'Qlfrtip',
-	// 			// DD-1566
-	// 			// destroy: true,
-	// 			searchBuilder: {
-	// 				preDefined: {
-	// 					criteria: [
-	// 						{
-	// 							condition: 'Greater Than Equal To',
-	// 							data: 'Age',
-	// 							value: ['66']
-	// 						}
-	// 					],
-	// 					logic: 'AND'
-	// 				}
-	// 			}
-	// 		});
+	describe('Date tests', function() {
+		dt.html('basic');
+		it('... Before', function() {
+			table = $('#example').DataTable({
+				dom: 'Qlfrtip',
+				searchBuilder: {
+					preDefined: {
+						criteria: [
+							{
+								condition: 'Before',
+								data: 'Start date',
+								value: ['2009-01-13']
+							}
+						],
+						logic: 'AND'
+					}
+				}
+			});
 
-	// 		expect($('#example tbody tr').length).toBe(2);
-	// 		expect($('#example tbody tr td:eq(0)').text()).toBe('Ashton Cox');
-	// 	});
+			expect($('#example tbody tr').length).toBe(10);
+			expect($('#example tbody tr:eq(1) td:eq(0)').text()).toBe('Ashton Cox');
+			expect($('#example tbody tr:eq(9) td:eq(0)').text()).toBe('Timothy Mooney');
+		});
+		it('... After', function() {
+			table = $('#example').DataTable({
+				dom: 'Qlfrtip',
+				destroy: true,
+				searchBuilder: {
+					preDefined: {
+						criteria: [
+							{
+								condition: 'After',
+								data: 'Start date',
+								value: ['2013-01-13']
+							}
+						],
+						logic: 'AND'
+					}
+				}
+			});
 
-	// 	dt.html('basic'); //DD-1566
-	// 	it('... Greater than', function() {
-	// 		// DD-1566
-	// 		$('#example tbody tr:eq(2) td:eq(2)').text('');
-	// 		table = $('#example').DataTable({
-	// 			dom: 'Qlfrtip',
-	// 			// DD-1566
-	// 			// destroy: true,
-	// 			searchBuilder: {
-	// 				preDefined: {
-	// 					criteria: [
-	// 						{
-	// 							condition: 'Greater Than',
-	// 							data: 'Age',
-	// 							value: ['66']
-	// 						}
-	// 					],
-	// 					logic: 'AND'
-	// 				}
-	// 			}
-	// 		});
+			expect($('#example tbody tr').length).toBe(3);
+			expect($('#example tbody tr:eq(0) td:eq(0)').text()).toBe('Jennifer Acosta');
+			expect($('#example tbody tr:eq(2) td:eq(0)').text()).toBe('Thor Walton');
+		});
 
-	// 		expect($('#example tbody tr').length).toBe(1);
-	// 		expect($('#example tbody tr').text()).toBe('No matching records found');
-	// 	});
-	// });
+		// DD-1568
 
-	// describe('Date tests', function() {
-	// 	dt.html('basic');
-	// 	it('... Before', function() {
-	// 		table = $('#example').DataTable({
-	// 			dom: 'Qlfrtip',
-	// 			searchBuilder: {
-	// 				preDefined: {
-	// 					criteria: [
-	// 						{
-	// 							condition: 'Before',
-	// 							data: 'Start date',
-	// 							value: ['2009-01-13']
-	// 						}
-	// 					],
-	// 					logic: 'AND'
-	// 				}
-	// 			}
-	// 		});
+		it('... Between', function() {
+			table = $('#example').DataTable({
+				dom: 'Qlfrtip',
+				destroy: true,
+				searchBuilder: {
+					preDefined: {
+						criteria: [
+							{
+								condition: 'Between',
+								data: 'Start date',
+								value: ['2009-09-13', '2009-01-12']
+							}
+						],
+						logic: 'AND'
+					}
+				}
+			});
 
-	// 		expect($('#example tbody tr').length).toBe(10);
-	// 		expect($('#example tbody tr:eq(1) td:eq(0)').text()).toBe('Ashton Cox');
-	// 		expect($('#example tbody tr:eq(9) td:eq(0)').text()).toBe('Timothy Mooney');
-	// 	});
+			expect($('#example tbody tr').length).toBe(7);
+			expect($('#example tbody tr:eq(0) td:eq(0)').text()).toBe('Ashton Cox');
+			expect($('#example tbody tr:eq(6) td:eq(0)').text()).toBe('Yuri Berry');
+		});
+	});
 
-	// 	dt.html('basic');
-	// 	it('... After', function() {
-	// 		table = $('#example').DataTable({
-	// 			dom: 'Qlfrtip',
-	// 			searchBuilder: {
-	// 				preDefined: {
-	// 					criteria: [
-	// 						{
-	// 							condition: 'After',
-	// 							data: 'Start date',
-	// 							value: ['2013-01-13']
-	// 						}
-	// 					],
-	// 					logic: 'AND'
-	// 				}
-	// 			}
-	// 		});
+	describe('HTML tests', function() {
+		dt.html('html');
+		it('... Equals', function() {
+			table = $('#example').DataTable({
+				dom: 'Qlfrtip',
+				searchBuilder: {
+					preDefined: {
+						criteria: [
+							{
+								condition: 'Equals',
+								data: 'HTML',
+								value: ['$86,000']
+							}
+						],
+						logic: 'AND'
+					}
+				}
+			});
 
-	// 		expect($('#example tbody tr').length).toBe(3);
-	// 		expect($('#example tbody tr:eq(0) td:eq(0)').text()).toBe('Jennifer Acosta');
-	// 		expect($('#example tbody tr:eq(2) td:eq(0)').text()).toBe('Thor Walton');
-	// 	});
+			expect($('#example tbody tr').length).toBe(1);
+			expect($('#example tbody tr:eq(0) td:eq(0)').text()).toBe('Ashton Cox');
+		});
+		it('... Between', function() {
+			table = $('#example').DataTable({
+				dom: 'Qlfrtip',
+				destroy: true,
+				searchBuilder: {
+					preDefined: {
+						criteria: [
+							{
+								condition: 'Between',
+								data: 'HTML',
+								value: ['86000', '90000']
+							}
+						],
+						logic: 'AND'
+					}
+				}
+			});
 
-	// 	// DD-1568
-	// 	// dt.html('basic');
-	// 	// it('... Between', function() {
-	// 	// 	table = $('#example').DataTable({
-	// 	// 		dom: 'Qlfrtip',
-	// 	// 		searchBuilder: {
-	// 	// 			preDefined: {
-	// 	// 				criteria: [
-	// 	// 					{
-	// 	// 						condition: 'Between',
-	// 	// 						data: 'Start date',
-	// 	// 						value: ['2009-09-13', '2009-01-12']
-	// 	// 					}
-	// 	// 				],
-	// 	// 				logic: 'AND'
-	// 	// 			}
-	// 	// 		}
-	// 	// 	});
-
-	// 	// 	expect($('#example tbody tr').length).toBe(7);
-	// 	// 	expect($('#example tbody tr:eq(0) td:eq(0)').text()).toBe('Ashton Cox');
-	// 	// 	expect($('#example tbody tr:eq(2) td:eq(0)').text()).toBe('Yuri Berry');
-	// 	// });
-	// });
-
-	// describe('HTML tests', function() {
-	// 	dt.html('html');
-	// 	it('... Equals', function() {
-	// 		table = $('#example').DataTable({
-	// 			dom: 'Qlfrtip',
-	// 			searchBuilder: {
-	// 				preDefined: {
-	// 					criteria: [
-	// 						{
-	// 							condition: 'Equals',
-	// 							data: 'HTML',
-	// 							value: ['$86,000']
-	// 						}
-	// 					],
-	// 					logic: 'AND'
-	// 				}
-	// 			}
-	// 		});
-
-	// 		expect($('#example tbody tr').length).toBe(1);
-	// 		expect($('#example tbody tr:eq(0) td:eq(0)').text()).toBe('Ashton Cox');
-	// 	});
-	// });
+			expect($('#example tbody tr').length).toBe(3);
+			expect($('#example tbody tr:eq(0) td:eq(0)').text()).toBe('Ashton Cox');
+			expect($('#example tbody tr:eq(2) td:eq(0)').text()).toBe('Jonas Alexander');
+		});
+	});
 });
