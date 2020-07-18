eccdc91f5778e4fc083a6cb7549d0441e3c88b9f
Colin Marks <colin@datatables.net>
2020-07-18 08:38:09 +0100

    DEV added tests for colreorder

--- test/api/searchBuilder.rebuild().js
+++ test/api/searchBuilder.rebuild().js
@@ -2,8 +2,8 @@ describe('searchBuilder - API - searchBuilder.rebuild()', function() {
 	let table;
 
 	dt.libs({
-		js: ['jquery', 'datatables', 'searchbuilder', 'colreorder'],
-		css: ['datatables', 'searchbuilder', 'colreorder']
+		js: ['jquery', 'datatables', 'searchbuilder'],
+		css: ['datatables', 'searchbuilder']
 	});
 
 	describe('Check the defaults', function() {
@@ -62,30 +62,6 @@ describe('searchBuilder - API - searchBuilder.rebuild()', function() {
 			expect($('.dtsb-data:eq(0) option:selected').text()).toBe('Office');
 			expect($('.dtsb-data:eq(1) option:selected').text()).toBe('Age');
 			expect($('tbody tr:eq(0) td:eq(0)').text()).toBe('Ashton Cox');
-			expect($('tbody tr').length).toBe(2);
-		});
-
-		dt.html('basic');
-		it('ColReorder', function() {
-			table = $('#example').DataTable({
-				dom: 'Qlfrtip',
-				colReorder: true
-			});
-
-			table.searchBuilder.rebuild({
-				criteria: [
-					{ condition: '=', data: 'Office', dataIdx: '2', value: ['San Francisco'] },
-					{ condition: '>', data: 'Age', dataIdx: '3', value: [60] }
-				],
-				logic: 'AND'
-			});
-
-			table.colReorder.move(3, 0);
-
-			expect($('.dtsb-criteria').length).toBe(2);
-			expect($('.dtsb-data:eq(0) option:selected').text()).toBe('Office');
-			expect($('.dtsb-data:eq(1) option:selected').text()).toBe('Age');
-			expect($('tbody tr:eq(0) td:eq(0)').text()).toBe('66');
 			expect($('tbody tr').length).toBe(2);
 		});
 	});
