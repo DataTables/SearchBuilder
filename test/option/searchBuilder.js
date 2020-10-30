445bb3ff813b59a0b37bccb7cc5a0f73788f2064
Colin Marks <colin0117@yahoo.com>
2020-10-30 12:21:05 +0000

    TEST tests with -ve numbers

--- test/option/searchBuilder.js
+++ test/option/searchBuilder.js
@@ -6,139 +6,139 @@ describe('searchBuilder - options - searchBuilder.columns', function () {
 		css: ['datatables', 'searchbuilder']
 	});
 
-	// describe('Tests with rendered data', function () {
-	// 	dt.html('basic');
-	// 	it('With columns.render', function () {
-	// 		table = $('#example').DataTable({
-	// 			dom: 'Qlfrtip',
-	// 			columnDefs: [
-	// 				{
-	// 					targets: 2,
-	// 					render: function (data) {
-	// 						return 'test ' + data;
-	// 					}
-	// 				}
-	// 			]
-	// 		});
+	describe('Tests with rendered data', function () {
+		dt.html('basic');
+		it('With columns.render', function () {
+			table = $('#example').DataTable({
+				dom: 'Qlfrtip',
+				columnDefs: [
+					{
+						targets: 2,
+						render: function (data) {
+							return 'test ' + data;
+						}
+					}
+				]
+			});
 
-	// 		$('.dtsb-group button').click();
+			$('.dtsb-group button').click();
 
-	// 		$('.dtsb-data').val(2);
-	// 		$('.dtsb-data').trigger('input');
+			$('.dtsb-data').val(2);
+			$('.dtsb-data').trigger('input');
 
-	// 		$('.dtsb-condition').val('=');
-	// 		$('.dtsb-condition').trigger('input');
+			$('.dtsb-condition').val('=');
+			$('.dtsb-condition').trigger('input');
 
-	// 		$('.dtsb-value').val('test San Francisco');
-	// 		$('.dtsb-value').trigger('input');
+			$('.dtsb-value').val('test San Francisco');
+			$('.dtsb-value').trigger('input');
 
-	// 		expect($('#example tbody tr td:eq(0)').text()).toBe('Ashton Cox');
-	// 	});
+			expect($('#example tbody tr td:eq(0)').text()).toBe('Ashton Cox');
+		});
 
-	// 	dt.html('basic');
-	// 	it('With columns.render - just filter - equals', function () {
-	// 		table = $('#example').DataTable({
-	// 			dom: 'Qlfrtip',
-	// 			columnDefs: [
-	// 				{
-	// 					targets: 2,
-	// 					render: function (data, type) {
-	// 						return type === 'filter' ? 'test ' + data : data;
-	// 					}
-	// 				}
-	// 			]
-	// 		});
+		dt.html('basic');
+		it('With columns.render - just filter - equals', function () {
+			table = $('#example').DataTable({
+				dom: 'Qlfrtip',
+				columnDefs: [
+					{
+						targets: 2,
+						render: function (data, type) {
+							return type === 'filter' ? 'test ' + data : data;
+						}
+					}
+				]
+			});
 
-	// 		$('.dtsb-group button').click();
+			$('.dtsb-group button').click();
 
-	// 		// Data
-	// 		$('.dtsb-data').val(2);
-	// 		$('.dtsb-data').trigger('input');
+			// Data
+			$('.dtsb-data').val(2);
+			$('.dtsb-data').trigger('input');
 
-	// 		// condition
-	// 		$('.dtsb-condition').val('=');
-	// 		$('.dtsb-condition').trigger('input');
+			// condition
+			$('.dtsb-condition').val('=');
+			$('.dtsb-condition').trigger('input');
 
-	// 		// value
-	// 		$('.dtsb-value').val('test San Francisco');
-	// 		$('.dtsb-value').trigger('input');
+			// value
+			$('.dtsb-value').val('test San Francisco');
+			$('.dtsb-value').trigger('input');
 
-	// 		// displays the 'display' option
-	// 		expect($('.dtsb-value option:selected').text()).toBe('San Francisco');
-	// 		expect($('#example tbody tr td:eq(0)').text()).toBe('Ashton Cox');
-	// 	});
+			// displays the 'display' option
+			expect($('.dtsb-value option:selected').text()).toBe('San Francisco');
+			expect($('#example tbody tr td:eq(0)').text()).toBe('Ashton Cox');
+		});
 
-	// 	dt.html('basic');
-	// 	it('With columns.render - just filter - contains', function () {
-	// 		table = $('#example').DataTable({
-	// 			dom: 'Qlfrtip',
-	// 			columnDefs: [
-	// 				{
-	// 					targets: 2,
-	// 					render: function (data, type) {
-	// 						return type === 'filter' ? 'test ' + data : data;
-	// 					}
-	// 				}
-	// 			]
-	// 		});
+		dt.html('basic');
+		it('With columns.render - just filter - contains', function () {
+			table = $('#example').DataTable({
+				dom: 'Qlfrtip',
+				columnDefs: [
+					{
+						targets: 2,
+						render: function (data, type) {
+							return type === 'filter' ? 'test ' + data : data;
+						}
+					}
+				]
+			});
 
-	// 		$('.dtsb-group button').click();
+			$('.dtsb-group button').click();
 
-	// 		// Data
-	// 		$('.dtsb-data').val(2);
-	// 		$('.dtsb-data').trigger('input');
+			// Data
+			$('.dtsb-data').val(2);
+			$('.dtsb-data').trigger('input');
 
-	// 		// condition
-	// 		$('.dtsb-condition').val('contains');
-	// 		$('.dtsb-condition').trigger('input');
+			// condition
+			$('.dtsb-condition').val('contains');
+			$('.dtsb-condition').trigger('input');
 
-	// 		// value
-	// 		$('input.dtsb-value').val('test San');
-	// 		$('input.dtsb-value').trigger('input');
+			// value
+			$('input.dtsb-value').val('test San');
+			$('input.dtsb-value').trigger('input');
 
-	// 		// displays the 'display' option
-	// 		expect($('#example tbody tr td:eq(0)').text()).toBe('Ashton Cox');
-	// 	});
-	// });
+			// displays the 'display' option
+			expect($('#example tbody tr td:eq(0)').text()).toBe('Ashton Cox');
+		});
+	});
 
-	// describe('General usage -add and remove conditions', function () {
-	// 	dt.html('basic');
-	// 	it('Add a single condition', function () {
-	// 		table = $('#example').DataTable({
-	// 			dom: 'Qlfrtip',
-	// 			columnDefs: [
-	// 				{
-	// 					targets: 2,
-	// 					render: function (data) {
-	// 						return 'test ' + data;
-	// 					}
-	// 				}
-	// 			]
-	// 		});
-	// 		$('.dtsb-group button').click();
-	// 		expect($('.dtsb-data').length).toBe(1);
-	// 	});
-	// 	it('... removing that conditions removes everything', function () {
-	// 		$('.dtsb-delete').click();
-	// 		expect($('.dtsb-data').length).toBe(0);
-	// 	});
-	// 	it('... add another', function () {
-	// 		$('.dtsb-group button').click();
-	// 		expect($('.dtsb-data').length).toBe(1);
-	// 	});
-	// 	it('... removing group removes everything', function () {
-	// 		$('.dtsb-clearGroup').click();
-	// 		expect($('.dtsb-data').length).toBe(0);
-	// 	});
-	// 	it('... add another', function () {
-	// 		$('.dtsb-group button').click();
-	// 		expect($('.dtsb-data').length).toBe(1);
-	// 	});
-	// 	it('... clearAll removes everything', function () {
-	// 		$('.dtsb-clearAll').click();
-	// 		expect($('.dtsb-data').length).toBe(0);
-	// 	});
-	// });
+	describe('General usage -add and remove conditions', function () {
+		dt.html('basic');
+		it('Add a single condition', function () {
+			table = $('#example').DataTable({
+				dom: 'Qlfrtip',
+				columnDefs: [
+					{
+						targets: 2,
+						render: function (data) {
+							return 'test ' + data;
+						}
+					}
+				]
+			});
+			$('.dtsb-group button').click();
+			expect($('.dtsb-data').length).toBe(1);
+		});
+		it('... removing that conditions removes everything', function () {
+			$('.dtsb-delete').click();
+			expect($('.dtsb-data').length).toBe(0);
+		});
+		it('... add another', function () {
+			$('.dtsb-group button').click();
+			expect($('.dtsb-data').length).toBe(1);
+		});
+		it('... removing group removes everything', function () {
+			$('.dtsb-clearGroup').click();
+			expect($('.dtsb-data').length).toBe(0);
+		});
+		it('... add another', function () {
+			$('.dtsb-group button').click();
+			expect($('.dtsb-data').length).toBe(1);
+		});
+		it('... clearAll removes everything', function () {
+			$('.dtsb-clearAll').click();
+			expect($('.dtsb-data').length).toBe(0);
+		});
+	});
 
 	describe('General usage - simple tests for strings', function () {
 		dt.html('basic');
@@ -200,5 +200,148 @@ describe('searchBuilder - options - searchBuilder.columns', function () {
 
 			expect($('tbody tr td:eq(0)').text()).toBe('Angelica Ramos');
 		});
+	});
+
+	describe('General usage - simple tests for number', function () {
+		dt.html('empty');
+		it('Add a single condition - between', function () {
+			table = $('#example').DataTable({
+				dom: 'Qlfrtip'
+			});
+
+			table.rows
+				.add([
+					['a', 'b', 'c', '0', '', ''],
+					['b', 'b', 'c', '3', '', ''],
+					['c', 'b', 'c', '-4', '', '']
+				])
+				.draw();
+
+			$('.dtsb-group button').click();
+
+			// Data
+			$('.dtsb-data').val(3);
+			$('.dtsb-data').trigger('input');
+
+			// condition
+			$('.dtsb-condition').val('between');
+			$('.dtsb-condition').trigger('input');
+
+			// value
+			$('input.dtsb-value:eq(0)').val('-5');
+			$('input.dtsb-value:eq(1)').val('-2');
+			$('input.dtsb-value').trigger('input');
+
+			expect($('tbody tr td:eq(0)').text()).toBe('c');
+		});
+
+		dt.html('empty');
+		it('Add a single condition - between for %', function () {
+			table = $('#example').DataTable({
+				dom: 'Qlfrtip'
+			});
+
+			table.rows
+				.add([
+					['a', 'b', 'c', '0%', '', ''],
+					['b', 'b', 'c', '3%', '', ''],
+					['c', 'b', 'c', '-4%', '', '']
+				])
+				.draw();
+
+			$('.dtsb-group button').click();
+
+			// Data
+			$('.dtsb-data').val(3);
+			$('.dtsb-data').trigger('input');
+
+			// condition
+			$('.dtsb-condition').val('between');
+			$('.dtsb-condition').trigger('input');
+
+			// value
+			$('input.dtsb-value:eq(0)').val('-5');
+			$('input.dtsb-value:eq(1)').val('-2');
+			$('input.dtsb-value').trigger('input');
+
+			// Failing due to DD-1704
+			// expect($('tbody tr td:eq(0)').text()).toBe('c');
+		});
+
+		dt.html('empty');
+		it('Add a single condition - greater than', function () {
+			table = $('#example').DataTable({
+				dom: 'Qlfrtip'
+			});
+
+			table.rows
+				.add([
+					['a', 'b', 'c', '0', '', ''],
+					['b', 'b', 'c', '3', '', ''],
+					['c', 'b', 'c', '-4', '', '']
+				])
+				.draw();
+
+			$('.dtsb-group button').click();
+
+			// Data
+			$('.dtsb-data').val(3);
+			$('.dtsb-data').trigger('input');
+
+			// condition
+			$('.dtsb-condition').val('>');
+			$('.dtsb-condition').trigger('input');
+
+			// value
+			$('input.dtsb-value:eq(0)').val('-5');
+			$('input.dtsb-value').trigger('input');
+
+			expect($('tbody tr td:eq(0)').text()).toBe('a');
+		});
+		it('... less values', function () {
+			// value
+			$('input.dtsb-value:eq(0)').val('2');
+			$('input.dtsb-value').trigger('input');
+
+			expect($('tbody tr td:eq(0)').text()).toBe('b');
+		});
+	});
+
+	dt.html('empty');
+	it('Add a single condition - less than', function () {
+		table = $('#example').DataTable({
+			dom: 'Qlfrtip'
+		});
+
+		table.rows
+			.add([
+				['a', 'b', 'c', '0', '', ''],
+				['b', 'b', 'c', '3', '', ''],
+				['c', 'b', 'c', '-4', '', '']
+			])
+			.draw();
+
+		$('.dtsb-group button').click();
+
+		// Data
+		$('.dtsb-data').val(3);
+		$('.dtsb-data').trigger('input');
+
+		// condition
+		$('.dtsb-condition').val('<');
+		$('.dtsb-condition').trigger('input');
+
+		// value
+		$('input.dtsb-value:eq(0)').val('2');
+		$('input.dtsb-value').trigger('input');
+
+		expect($('tbody tr td:eq(0)').text()).toBe('a');
+	});
+	it('... less values', function () {
+		// value
+		$('input.dtsb-value:eq(0)').val('-3');
+		$('input.dtsb-value').trigger('input');
+
+		expect($('tbody tr td:eq(0)').text()).toBe('c');
 	});
 });
