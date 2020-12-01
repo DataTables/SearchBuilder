157724d9ce8193cb7a959a871de3b16d9aec73b5
Allan Jardine <allan@datatables.net>
2020-12-01 11:56:28 +0000

    Dev: Ready for 1.0.1

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-/*! SearchBuilder 1.0.0
+/*! SearchBuilder 1.0.1
  * ©2020 SpryMedia Ltd - datatables.net/license/mit
  */
 
 /**
  * @summary     SearchBuilder
  * @description End user complex search builder for DataTables
- * @version     1.0.0
+ * @version     1.0.1
  * @file        dataTables.searchBuilder.js
  * @author      SpryMedia Ltd
  * @copyright   Copyright 2020 SpryMedia Ltd.
