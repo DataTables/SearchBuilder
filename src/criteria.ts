674cc02ab075f56c6dfbd4df3b951a7521cda371
SandyDatatables <sandy@datatables.net>
2021-02-12 11:36:44 +0000

    fix: Fix integration issue with different language options for decimal and thousands characters.
    Jira Issue DD-1882

--- src/criteria.ts
+++ src/criteria.ts
@@ -1682,15 +1682,29 @@ export default class Criteria {
 		let condition = this.s.conditions[this.s.condition];
 
 		if (this.s.condition !== undefined &&  condition !== undefined) {
+			let filter = rowData[this.s.dataIdx];
 			// This check is in place for if a custom decimal character is in place
-			if (this.s.type.indexOf('num') !== -1 && this.s.dt.settings()[0].oLanguage.sDecimal !== '') {
-				rowData[this.s.dataIdx] = rowData[this.s.dataIdx].replace(this.s.dt.settings()[0].oLanguage.sDecimal, '.');
+			if (
+				this.s.type.indexOf('num') !== -1 &&
+				(this.s.dt.settings()[0].oLanguage.sDecimal !== '' || this.s.dt.settings()[0].oLanguage.sThousands !== '')
+			) {
+
+				let splitRD = [rowData[this.s.dataIdx]];
+				if (this.s.dt.settings()[0].oLanguage.sDecimal !== '') {
+					splitRD = rowData[this.s.dataIdx].split(this.s.dt.settings()[0].oLanguage.sDecimal);
+				}
+
+				if (this.s.dt.settings()[0].oLanguage.sThousands !== '') {
+					for (let i = 0; i < splitRD.length; i++) {
+						splitRD[i] = splitRD[i].replace(this.s.dt.settings()[0].oLanguage.sThousands, ',');
+					}
+				}
+
+				filter = splitRD.join('.');
 			}
 
-			let filter = rowData[this.s.dataIdx];
-
 			// If orthogonal data is in place we need to get it's values for searching
-			if (this.c.orthogonal.search !== 'search') {
+			if (this.c.orthogonal.search !== 'filter') {
 				let settings = this.s.dt.settings()[0];
 
 				filter = settings.oApi._fnGetCellData(
