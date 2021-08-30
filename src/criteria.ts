e5493688576d0f01f802854bc1032118bf4b0695
SandyDatatables <sandy@datatables.net>
2021-08-30 09:09:41 +0000

    fix: Fix issue with columns.searchBuilderType not taking priority for the column type
    Jira Issue DD-2141

--- src/criteria.ts
+++ src/criteria.ts
@@ -2450,11 +2450,17 @@ export default class Criteria {
 			let column = +this.dom.data.children('option:selected').val();
 			this.s.type = this.s.dt.columns().type().toArray()[column];
 
-			if (this.s.type === undefined || this.s.type === null) {
-				let colInit = this.s.dt.init().aoColumns[column];
-				this.s.type = colInit.searchBuilderType !== undefined ? colInit.searchBuilderType : colInit.sType;
+			let colInits = this.s.dt.settings()[0].aoColumns;
+			if(colInits !== undefined) {
+				let colInit = colInits[column];
+				if(colInit.searchBuilderType !== undefined && colInit.searchBuilderType !== null) {
+					this.s.type = colInit.searchBuilderType;
+				}
+				else if (this.s.type === undefined || this.s.type === null) {
+					this.s.type = colInit.sType;
+				}
 			}
-			// If the column type is unknown, call a draw to try reading it again
+			// If the column type is still unknown, call a draw to try reading it again
 			if (this.s.type === null || this.s.type === undefined) {
 				$.fn.dataTable.ext.oApi._fnColumnTypes(this.s.dt.settings()[0]);
 				this.s.type = this.s.dt.columns().type().toArray()[column];
