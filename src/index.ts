ae8f5707cd624f119e15cf70cde815cf02ee623c
Allan Jardine <allan@datatables.net>
2021-10-25 11:03:33 +0000

    Release 1.3.0

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-/*! SearchBuilder 1.2.2
+/*! SearchBuilder 1.3.0
  * ©SpryMedia Ltd - datatables.net/license/mit
  */
 
 /**
  * @summary     SearchBuilder
  * @description End user complex search builder for DataTables
- * @version     1.2.2
+ * @version     1.3.0
  * @file        dataTables.searchBuilder.js
  * @author      SpryMedia Ltd
  * @copyright   Copyright SpryMedia Ltd.
--- src/searchBuilder.ts
+++ src/searchBuilder.ts
@@ -91,7 +91,7 @@ export interface IS {
  * Allows for complex search queries to be constructed and implemented on a DataTable
  */
 export default class SearchBuilder {
-	private static version = '1.2.2';
+	private static version = '1.3.0';
 
 	private static classes: IClasses = {
 		button: 'dtsb-button',
