0cf345a93d31b80c3090e002204c9cd70ebee266
Allan Jardine <allan@datatables.net>
2022-06-17 09:44:18 +0000

    SearchBuilder 1.3.4

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-/*! SearchBuilder 1.3.3
+/*! SearchBuilder 1.3.4
  * ©SpryMedia Ltd - datatables.net/license/mit
  */
 
 /**
  * @summary     SearchBuilder
  * @description End user complex search builder for DataTables
- * @version     1.3.3
+ * @version     1.3.4
  * @author      SpryMedia Ltd
  * @copyright   Copyright SpryMedia Ltd.
  *
