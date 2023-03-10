fc0e19d74da50f4629cc620fddf993a94622d4cd
Allan Jardine <allan@datatables.net>
2023-03-10 16:54:50 +0000

    1.4.1 release

--- src/searchBuilder.ts
+++ src/searchBuilder.ts
@@ -97,7 +97,7 @@ export interface IS {
  * Allows for complex search queries to be constructed and implemented on a DataTable
  */
 export default class SearchBuilder {
-	private static version = '1.4.0';
+	private static version = '1.4.1';
 
 	private static classes: IClasses = {
 		button: 'dtsb-button',
