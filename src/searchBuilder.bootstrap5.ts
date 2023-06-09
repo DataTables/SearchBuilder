a05244a7c2d7c499525b454760968b78dbd873ca
Allan Jardine <allan@datatables.net>
2023-06-09 16:58:39 +0100

    New: Dark mode support for DataTables and Bootstrap 5.3+ styling

--- src/searchBuilder.bootstrap5.ts
+++ src/searchBuilder.bootstrap5.ts
@@ -5,22 +5,22 @@
 declare var DataTable: any;
 
 $.extend(true, DataTable.SearchBuilder.classes, {
-	clearAll: 'btn btn-light dtsb-clearAll'
+	clearAll: 'btn btn-secondary dtsb-clearAll'
 });
 
 $.extend(true, DataTable.Group.classes, {
-	add: 'btn btn-light dtsb-add',
-	clearGroup: 'btn btn-light dtsb-clearGroup',
-	logic: 'btn btn-light dtsb-logic'
+	add: 'btn btn-secondary dtsb-add',
+	clearGroup: 'btn btn-secondary dtsb-clearGroup',
+	logic: 'btn btn-secondary dtsb-logic'
 });
 
 $.extend(true, DataTable.Criteria.classes, {
 	condition: 'form-select dtsb-condition',
 	data: 'dtsb-data form-select',
-	delete: 'btn btn-light dtsb-delete',
+	delete: 'btn btn-secondary dtsb-delete',
 	input: 'form-control dtsb-input',
-	left: 'btn btn-light dtsb-left',
-	right: 'btn btn-light dtsb-right',
+	left: 'btn btn-secondary dtsb-left',
+	right: 'btn btn-secondary dtsb-right',
 	select: 'form-select',
 	value: 'dtsb-value',
 });
