15d9e359bb4d62dbf5449d2ee4666dba7207885f
Allan Jardine <allan@datatables.net>
2023-10-17 10:13:56 +0100

    Fix - typescript: Explicit void return type for new constructor

--- types/types.d.ts
+++ types/types.d.ts
@@ -65,7 +65,7 @@ declare module 'datatables.net' {
 			/**
 			 * Create a new SearchBuilder instance for the target DataTable
 			 */
-			new (dt: Api<any>, settings: string[] | ConfigSearchBuilder | ConfigSearchBuilder[]);
+			new (dt: Api<any>, settings: string[] | ConfigSearchBuilder | ConfigSearchBuilder[]): void;
 
 			/**
 			 * SearchBuilder version
