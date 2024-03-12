b1ce7f3607f605765514fa2b4c32f6652294c611
Naoki Miyazaki-Chapleau <104367522+nmiyazaki-chapleau@users.noreply.github.com>
2024-03-12 08:57:53 -0400

    Move "search" to Group instead of Criteria

--- src/searchBuilder.foundation.ts
+++ src/searchBuilder.foundation.ts
@@ -12,6 +12,7 @@ $.extend(true, DataTable.Group.classes, {
 	add: 'button dtsb-add',
 	clearGroup: 'button dtsb-clearGroup',
 	logic: 'button dtsb-logic',
+	search: 'button dtsb-search',
 });
 
 $.extend(true, DataTable.Criteria.classes, {
@@ -20,6 +21,5 @@ $.extend(true, DataTable.Criteria.classes, {
 	delete: 'button alert dtsb-delete',
 	left: 'button dtsb-left',
 	right: 'button dtsb-right',
-	search: 'button dtsb-search',
 	value: 'form-control dtsb-value',
 });
