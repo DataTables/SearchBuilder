5e4faaa8acdc6947dbdad8667335cc52083aa645
Allan Jardine <allan@datatables.net>
2026-02-25 09:49:06 +0000

    Test: Renable and fix tests (wrong condition used)

--- test/option/searchBuilder.preDefined.criteria.js
+++ test/option/searchBuilder.preDefined.criteria.js
@@ -1,4 +1,4 @@
-describe('searchBuilder - options - searchBuilder.preDefined.criteria', function() {
+describe('searchBuilder - options - searchBuilder.preDefined.criteria', function () {
 	let table;
 
 	dt.libs({
@@ -7,88 +7,84 @@ describe('searchBuilder - options - searchBuilder.preDefined.criteria', function
 	});
 
 	// Basic tests are in the other preDefined tests, so these are more complex
-	describe('Functional tests', function() {
-		// dt.html('basic');
-		// it('Nested criteria - single depth', function() {
-		// 	table = $('#example').DataTable({
-		// 		dom: 'Qlfrtip',
-		// 		searchBuilder: {
-		// 			preDefined: {
-		// 				criteria: [
-		// 					{
-		// 						criteria: [
-		// 							{
-		// 								condition: 'Not',
-		// 								data: 'Office',
-		// 								value: ['London']
-		// 							},
+	describe('Functional tests', function () {
+		dt.html('basic');
 
-		// 							{
-		// 								condition: 'Greater Than',
-		// 								data: 'Age',
-		// 								value: ['55']
-		// 							}
-		// 						]
-		// 					},
-		// 					{
-		// 						criteria: [
-		// 							{
-		// 								condition: 'Between',
-		// 								data: 'Salary',
-		// 								value: ['2', '10000000']
-		// 							},
+		it('Nested criteria - single depth', function () {
+			table = $('#example').DataTable({
+				dom: 'Qlfrtip',
+				searchBuilder: {
+					preDefined: {
+						criteria: [
+							{
+								criteria: [
+									{
+										condition: '!=',
+										data: 'Office',
+										value: ['London']
+									},
 
-		// 							// {
-		// 							// 	condition: 'Greater Than',
-		// 							// 	data: 'Age',
-		// 							// 	value: ['55']
-		// 							// }
-		// 						]
-		// 					},
+									{
+										condition: '>',
+										data: 'Age',
+										value: ['55']
+									}
+								]
+							},
+							{
+								criteria: [
+									{
+										condition: 'between',
+										data: 'Salary',
+										value: ['2', '10000000']
+									}
+								]
+							},
 
-		// 					{
-		// 						condition: 'Greater Than',
-		// 						data: 'Age',
-		// 						value: ['60']
-		// 					}
-		// 				],
-		// 				logic: 'AND'
-		// 			}
-		// 		}
-		// 	});
+							{
+								condition: '>',
+								data: 'Age',
+								value: ['60']
+							}
+						],
+						logic: 'AND'
+					}
+				}
+			});
 
-		// 	expect($('.dtsb-logic').text()).toBe('And');
-		// 	expect($('.dtsb-criteria').length).toBe(2);
-		// 	expect($('#example tbody tr td:eq(0)').text()).toBe('Ashton Cox');
-		// });
+			expect($('.dtsb-logic').eq(0).text()).toBe('And');
+			expect($('.dtsb-criteria').length).toBe(4);
+			expect($('#example tbody tr td:eq(0)').text()).toBe('Ashton Cox');
+		});
 
-		// dt.html('basic');
-		// it('Can specifiy OR', function() {
-		// 	table = $('#example').DataTable({
-		// 		dom: 'Qlfrtip',
-		// 		searchBuilder: {
-		// 			preDefined: {
-		// 				criteria: [
-		// 					{
-		// 						condition: 'Not',
-		// 						data: 'Office',
-		// 						value: ['London']
-		// 					},
+		dt.html('basic');
 
-		// 					{
-		// 						condition: 'Greater Than',
-		// 						data: 'Age',
-		// 						value: ['55']
-		// 					}
-		// 				],
-		// 				logic: 'OR'
-		// 			}
-		// 		}
-		// 	});
+		it('Can specifiy OR', function () {
+			table = $('#example').DataTable({
+				dom: 'Qlfrtip',
+				searchBuilder: {
+					preDefined: {
+						criteria: [
+							{
+								condition: '!=',
+								data: 'Office',
+								value: ['London']
+							},
 
-		// 	expect($('.dtsb-logic').text()).toBe('Or');
-		// 	expect($('.dtsb-criteria').length).toBe(2);
-		// 	expect($('#example tbody tr td:eq(0)').text()).toBe('Airi Satou');
-		// });
+							{
+								condition: '>',
+								data: 'Age',
+								value: ['55']
+							}
+						],
+						logic: 'OR'
+					}
+				}
+			});
+
+			expect($('.dtsb-logic').text()).toBe('Or');
+			expect($('.dtsb-criteria').length).toBe(2);
+			expect($('#example tbody tr td:eq(0)').text()).toBe('Airi Satou');
+		});
 	});
 });
